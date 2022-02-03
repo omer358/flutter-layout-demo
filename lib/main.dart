09eb9d109e546c02166d3590c7067fab8decb809
omer358 <omermaki358@gmail.com>
2022-02-03 16:34:05 +0200

    Step 3: Implement the button row

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+Widget buttonSection = Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: [
+    _buildButtonColumn(color, Icons.call, 'CALL'),
+    _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+    _buildButtonColumn(color, Icons.share, 'SHARE'),
+  ],
+);
+
     Widget titleSection = Container(
       padding: const EdgeInsets.all(24),
       child: Row(
@@ -48,6 +59,7 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    
     return MaterialApp(
       title: 'Flutter Layout Demo',
       theme: ThemeData(
@@ -58,8 +70,32 @@ class MyApp extends StatelessWidget {
             title: const Text("Flutter Layout Demo"),
           ),
           body: Column(
-            children: [titleSection],
+            children: [
+              titleSection,
+              buttonSection
+              ],
           )),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
